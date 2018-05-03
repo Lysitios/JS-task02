ab4c6742507c4b1668f60ce154f1e800d22d3633
Lysitios <31569966+Lysitios@users.noreply.github.com>
2018-05-03 21:53:55 +0800

    JStask02-new

--- JS-task02.js
+++ JS-task02.js
@@ -7,37 +7,20 @@ function lohref()
 function iptset() 
 {
 	var a=document.getElementById("iptnum").value;
-	if (a>=4 && a<=8) 
+	if (a>=4 && a<=18) 
 	{
-		document.getElementById("assin").innerHTML="杀手1人";
-		document.getElementById("civil").innerHTML="平民"+(a-1)+"人";
-		rdm(1);
+		var n=Math.floor(a/3);
+		document.getElementById("assin").innerHTML="杀手"+n+"人";
+		document.getElementById("civil").innerHTML="平民"+(a-n)+"人";
+		rdm(n);
 	}
-	    else if (a>=9 && a<=11) 
-	    {
-	        document.getElementById("assin").innerHTML="杀手2人";
-		    document.getElementById("civil").innerHTML="平民"+(a-2)+"人";
-		    rdm(2);
-	    }
-		    else if (a>=12 && a<=15) 
-		    {
-			    document.getElementById("assin").innerHTML="杀手3人";
-	            document.getElementById("civil").innerHTML="平民"+(a-3)+"人";
-	            rdm(3);
-		    }
-			    else if (a>=16 && a<=18) 
-			    {
-			    	document.getElementById("assin").innerHTML="杀手4人";
-	                document.getElementById("civil").innerHTML="平民"+(a-4)+"人";
-	                rdm(4);
-			    }
-			        else 
-			        {
-			        	document.getElementById("assin").innerHTML="杀手 人";
-	                    document.getElementById("civil").innerHTML="平民 人";
-			        	alert("请输入正确的玩家数量。");
-			        	document.getElementById("mblk").innerHTML="";
-			        }
+	else 
+	{
+		document.getElementById("assin").innerHTML="杀手 人";
+		document.getElementById("civil").innerHTML="平民 人";
+		document.getElementById("mblk").innerHTML="";
+		alert("请输入正确的玩家数量。");
+	}
 }
 
 function rdm(n)
